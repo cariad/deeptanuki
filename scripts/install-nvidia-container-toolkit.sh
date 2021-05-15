4c1e2d92fd5a18e4331fb8f29698c279b41f8f73
Cariad Eccleston <cariad@cariad.io>
2021-05-15 13:20:16 +0000

    Install nvidia-docker2

--- scripts/install-nvidia-container-toolkit.sh
+++ scripts/install-nvidia-container-toolkit.sh
@@ -7,5 +7,13 @@ distribution=$(. /etc/os-release;echo "${ID:?}${VERSION_ID:?}")
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L "https://nvidia.github.io/nvidia-docker/${distribution:?}/nvidia-docker.list" | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
+./scripts/update.sh
+
+echo -e "${li:?}Installing nvidia-docker2..."
+sudo apt install nvidia-docker2 --yes
+
+echo -e "${li:?}Restarting Docker..."
+sudo systemctl restart docker
+
 echo -e "${ok:?}NVIDIA Container Toolkit installed!"
 set +e
