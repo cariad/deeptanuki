88e6a75fb83d4153edf4afe3aea0ae8afff7a3b4
Cariad Eccleston <cariad@cariad.io>
2021-05-15 10:37:20 +0000

    Install CUDA toolkit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # deeptanuki
 Setup scripts for my DeepRacer training box
+
+## Installation
+
+```bash
+cd ~
+git clone https://github.com/cariad/deeptanuki.git
+cd deeptanuki
+./bootstrap.sh
+```
--- scripts/install-cuda-drivers.sh
+++ scripts/install-cuda-drivers.sh
@@ -1,7 +1,26 @@
 #!/usr/bin/env bash
 set -e
 
-echo -e "${li:?}TODO: Install CUDA drivers here."
+
+echo -e "${li:?}Installing Linux headers..."
+sudo apt install "linux-headers-$(uname -r)" --yes
+
+echo -e "${li:?}Downloading cuda-ubuntu2004.pin..."
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+
+echo -e "${li:?}Installing cuda-ubuntu2004.pin..."
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+
+echo -e "${li:?}Adding NVIDIA public key..."
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+
+echo -e "${li:?}Downloading NVIDIA repository..."
+sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+
+./scripts/update.sh
+
+echo -e "${li:?}Installing CUDA toolkit..."
+sudo apt install cuda --yes
 
 echo -e "${ok:?}CUDA drivers installed!"
 set +e
