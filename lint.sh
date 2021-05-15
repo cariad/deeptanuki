48dbede6ed7c9dc73f27834ee71179b91633e5c9
Cariad Eccleston <cariad@cariad.io>
2021-05-15 13:16:15 +0000

    Fix lint warnings

--- lint.sh
+++ lint.sh
@@ -6,7 +6,7 @@ set -e
 while IFS="" read -r file_path
 do
   echo -e "${li:?}${file_path:?}"
-  shellcheck --check-sourced --enable=all --severity style -x "${file_path:?}"
+  shellcheck --enable=all --severity style -x "${file_path:?}"
 done < <(find . -name "*.sh" -not -path "./.venv/*")
 
 echo -e "${ok:?}OK!"
