f51d7a1ae9fb31d4dd987f14599127840ebae91b
Cariad Eccleston <cariad@cariad.io>
2021-05-15 14:54:31 +0000

    Set LD_LIBRARY_PATH

--- data/bashrc.sh
+++ data/bashrc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 export PATH="${HOME}/.local/bin:/usr/local/cuda-11.3/bin${PATH:+:${PATH}}"
+export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu${PATH:+:${PATH}}"
 source ~/.deeptanuki/deepracer-for-cloud/bin/activate.sh
